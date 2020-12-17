47e35b9c61665bc2bd7b39efaa71c3317b85f424
Pelmeshka102 <arsenii_tishkov@mail.ru>
2020-12-17 11:39:04 +0500

    APP-103 Проблема CodeMagic 1) убрал все тесты, helthcheck и тд и тп, голый тест

--- test_driver/main.dart
+++ test_driver/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_driver_app/main.dart' as app;
 
 void main() {
+  enableFlutterDriverExtension();
   app.main();
 }
--- test_driver/main_test.dart
+++ test_driver/main_test.dart
@@ -6,21 +6,13 @@ void main() {
   setUpAll(() async {
     driver = await FlutterDriver.connect();
   });
-  test('Check health', () async {
-    var health = await driver.checkHealth();
-    expect(health.status, HealthStatus.ok);
-  });
-  group('MainPage', () {
-    test('Loading', () async {
-      await Future.delayed(Duration(seconds: 3));
-    });
-    test('Working', () async {
-      await Future.delayed(Duration(seconds: 3));
-    });
-  });
   tearDownAll(() async {
     if (driver != null) {
       await driver.close();
     }
+  });
+  group('MainPage', () {
+    test('Loading', () async {});
+    test('Working', () async {});
   });
 }
